d2fac6ba3468163cac9b5dbe9f7a4216986188e9
12521252 <mahmodabadi1398@gmail.com>
2021-11-28 22:42:34 +0330

    add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 
+import { useState } from 'react';
 import './App.css';
-import './assets/query/query.css';
-import Logo from './Component/logo/logoComponent';
-import Title from './Component/title/title';
-import Form from './Component/form/formComponent';
-import Weblog from './Component/weblog/weblog';
-import Main from './Component/main/main';
+import Form from './component/form';
+import Header from './component/header';
+import Table from './component/table';
+import Data from './Data';
 
 
 function App() {
-  return (
-    <div className="container">
-        <Logo/>
-    <div className="contact">
-        <Title/>
-        <Form/>
-    </div>    
-  <Weblog/>
-  <Main/>
+
+     const[data,setData]= useState(Data);
+     console.log(data);
+     return (
+    <div className="container-fluid">
+    <div className="row">
+      <div className="col-12 col-xs-12 col-sm-12 col-md-6">
+
+          <div className="main text-right">
+            <Header/>
+<Form data={data} setData={setData}/>
+
+          </div>
+
+      </div>
+      <div className="col-12 col-xs-12 col-sm-12 col-md-6 mx-auto mb-5">
+          <div style={{marginTop: "2rem", backgroundColor: "white" , fontSize:"1.3rem", padding: "4rem 2rem", borderRadius: "0.5rem"}}>
+             <Table data={data} setData={setData}/>
+          </div>
+      </div>
+  </div>
 </div>
+    
   );
 }
 
